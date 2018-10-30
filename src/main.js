0ac5fcb67c66f7de385316eb1acaae961f383c55
yfj <yefujieFrank@163.com>
2018-10-30 18:05:07 +0800

    初始化字体大小、颜色、自重

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '../static/css/style.scss'
 
 Vue.config.productionTip = false
 
---
+++ src/views/components/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <div class="logo">LOGO</div>
+    <ul class="nav">
+      <li><a href="">首页</a></li>
+      <li><a href="">记录</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  .logo {
+    height: 30px;
+    line-height: 30px;
+  }
+  .nav {
+    display: inline-block;
+  }
+}
+</style>
